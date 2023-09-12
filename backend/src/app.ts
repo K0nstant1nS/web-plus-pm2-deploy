0e8b1d5036593074e8f923091ba88f472edebe8c
Константин Cоловьев <K1kollov@gmail.com>
2023-09-12 11:56:39 +0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-app.use(cors());
+app.use(cors({ origin: 'https://mesto.ks.nomoredomainsicu.ru' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
