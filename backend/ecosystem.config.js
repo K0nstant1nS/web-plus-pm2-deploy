644a9c20c309ff410d138b3fb23ef150e0c1bb4c
Константин Cоловьев <K1kollov@gmail.com>
2023-09-12 15:00:02 +0300

    -

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_REPO, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
@@ -14,10 +14,10 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/K0nstant1nS/web-plus-pm2-deploy',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'local-pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend/ && npm i && npm run build`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend/ && npm i && npm run build && pm2 restart ./ecosystem.config.js`,
     },
   },
 };
