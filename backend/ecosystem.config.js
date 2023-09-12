bd5836190386f7fb8f953afdb1f53476387f6e3a
Константин Cоловьев <K1kollov@gmail.com>
2023-09-12 10:33:19 +0300

    -

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: 'https://github.com/K0nstant1nS/web-plus-pm2-deploy',
       path: DEPLOY_PATH,
       'local-pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd ~/mesto-back/source/backend/ && npm i && npm run build',
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend/ && npm i && npm run build`,
     },
   },
 };
